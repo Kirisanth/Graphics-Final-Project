08964085bb5097895726c604a770d064ad40843c
Meraj Patel <meraj0@Merajs-MacBook-Pro-2.local>
2013-12-05 04:09:08 -0500

    raycast

--- Graphics-Final-Project/main.cpp
+++ Graphics-Final-Project/main.cpp
@@ -11,6 +11,7 @@
 #include "MathLibrary.h"
 #include "Particle.h"
 #include <math.h>
+#include "ray.h"
 
 
 static int numOfParticles = 0;//count num of particles
@@ -25,14 +26,19 @@ bool frictionOnOff = false;//turn friction on and off
 bool wind = false;//turn wind on and off
 bool rainMode = false, snowMode = false, normalMode = true;//allocating modes for desired effects
 double camera[3] = {9,9,9};//declares camera at position
-//ray newRay;
+
+
+ray newRay;
 GLdouble newPoint [3];//creates new point
 GLdouble pNear[3];//depth for z
 GLdouble pFar[3]; //depth for z
 float objectPos[3];//stores object position
 float inter[3];//stores object intersection
 bool groundPlane = true;//check if hit on plane
-double platformNormal[4][3] ={{0,1,0}};
+double platformNormal[4][3] ={{0,1,0},{0,0,1},{0,-1,0},{1,0,0}};
+double platformPoints[4][3] = {{-1.5,-0.6,2.5}, {1.6,-0.7,2.5},{-1.5,-0.7,2.5},{-1.6,-0.7,2.5}};
+double minX = -1.5, maxX = 1.5, minY = -0.7, maxY = 0, minZ = -2.8, maxZ = 2.8;
+bool hit;
 
 
 
@@ -53,162 +59,93 @@ void Get3DPos(int x, int y, float winz, GLdouble point[3])
     
 }
 
+double distance(int i){
+    return -1 * (platformPoints[i][0]*platformNormal[i][0] + platformPoints[i][1]*platformNormal[i][1] + platformPoints[i][2]*platformNormal[i][2]);
+    
+}
 
+float normalMultiplyDirection(int i){
+    return (platformNormal[i][0] *  newRay.norm[0] + platformNormal[i][1] * newRay.norm[1] + platformNormal[i][2] * newRay.norm[2]);
+}
+
+float normalMultiplyOrgin(int i, float t){
+    return  (-1* (platformNormal[i][0] * newRay.org[0] + platformNormal[i][1] * newRay.org[1] + platformNormal[i][2] * newRay.org[2] + distance(i)))/t;
+}
 
 ////Function performs a ray plan test to check if ray intersected object other then sphere
-//bool rayPlaneTest(int i){
-//    float t = 0;
-//    //newRay.normalMultiplyDirection(i);
-//    t = (objectPlaneNormal[i][0] *  norm[0] + objectPlaneNormal[i][1] * norm[1] + objectPlaneNormal[i][2] * norm[2]);
-//    if (t != 0){
-//        t = newRay.normalMultiplyOrgin(i, t);
-//        //store instesection points if it did
-//        inter[0] = newRay.org[0] + t*newRay.norm[0];
-//        inter[1] = newRay.org[1] + t*newRay.norm[1];
-//        inter[2] = newRay.org[2] + t*newRay.norm[2];
-//        return true;
-//    }
-//    else {
-//        return false;
-//    }
-//    
-//}
+bool rayPlaneTest(int i){
+    float t = 0;
+    normalMultiplyDirection(i);
+    t = (platformNormal[i][0] *  newRay.norm[0] + platformNormal[i][1] * newRay.norm[1] + platformNormal[i][2] * newRay.norm[2]);
+    if (t != 0){
+        t = normalMultiplyOrgin(i, t);
+        //store instesection points if it did
+        inter[0] = newRay.org[0] + t*newRay.norm[0];
+        inter[1] = newRay.org[1] + t*newRay.norm[1];
+        inter[2] = newRay.org[2] + t*newRay.norm[2];
+
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+}
 
 //
 ///* rayCast - takes a mouse x,y, coordinate, and casts a ray through that point
 // *   for subsequent intersection tests with objects.
 // */
-//void rayCast(float x, float y)
-//{
-//    //count through number of objects to perform tests on each one
-//        //count through number of planes of each object to perform test on each one
-//        for (int i = 0; i < 4; i++){
-//            
-//            
-//            Get3DPos(x, y, 0.0, pNear);
-//            Get3DPos(x, y, 1.0, pFar);
-//            
-//            //store ray orgin
-//            newRay.org[0] = camera[0];
-//            newRay.org[1] = camera[1];
-//            newRay.org[2] = camera[2];
-//            
-//            //ray direction is the vector (pFar - pNear)
-//            newRay.dir[0] = pFar[0] - pNear[0];
-//            newRay.dir[1] = pFar[1] - pNear[1];
-//            newRay.dir[2] = pFar[2] - pNear[2];
-//            
-//            //normalize direction vector
-//            newRay.normalizeDirection();
-//            
-//            //undergoe ray plane test
-//            groundPlane = rayPlaneTest(i);
-//            
-//            //update the position of the object to the intersection point
-//            if ( groundPlane == true){
-//                objectPos[0] = inter[0];
-//                objectPos[1] = inter[1];
-//                objectPos[2] = inter[2];
-//                
-//                //check if object is hit between min and max bounds
-//                if ((objectsList[count].min + objectsList[count].translateX < objectPos[0] && objectPos[0] < objectsList[count].max + objectsList[count].translateX && objectsList[count].min + objectsList[count].translateZ < objectPos[2] && objectPos[2] < objectsList[count].max + objectsList[count].translateZ && objectPos[1] < objectsList[count].max + objectsList[count].translateY && objectsList[count].min + objectsList[count].translateY < objectPos[1])){
-//                    hit = true;
-//                    targetObject = count;
-//                    objectsList[count].hit = true;
-//                    //check to see right click to delete object
-//                    if (deleteObject == true){
-//                        objectsList[count].deleteObject();
-//                    }
-//                    break;
-//                }
-//                //return false hit else wise
-//                else{
-//                    hit = false;
-//                    objectsList[count].hit = false;
-//                }
-//                
-//                
-//            }
-//        }   //break out of cycle of object 
-//        if(hit == true){
-//            for(int z = 0; z < numOfObjects; z++){
-//                if(z != count){
-//                    objectsList[z].hit = false;
-//                }
-//            }
-//            break;
-//        }
-//}
+void rayCast(float x, float y)
 
-
-/*
- Used to draw actual platform where particles will held and manipulated with
- */
-
-void drawPlatform()
 {
-    
-	glBegin(GL_QUADS);
-    
-    //platform
-    glColor3f(1, 0, 0.3);
-    glVertex3f(-2.8,0,2.8);
-    glVertex3f(2.8,0,2.8);
-    glVertex3f(2.8,0,-2.8);
-    glVertex3f(-2.8,0,-2.8);
-    
-    //bottom
-    glColor3f(1, 0, 0.3);
-    glVertex3f(-2.8,-0.3,2.8);
-    glVertex3f(2.8,-0.3,2.8);
-    glVertex3f(2.8,-0.3,-2.8);
-    glVertex3f(-2.8,-0.3,-2.8);
-    
-    //front
-  	glColor3f(1, 0, 0.3);
-    glVertex3f(-2.8, -0.3, 2.8);
-    glVertex3f(2.8, -0.3, 2.8);
-    glVertex3f(2.8, 0, 2.8);
-    glVertex3f(-2.8, 0, 2.8);
-    
-    //left side
-    glColor3f(1, 0, 0.3);
-    glVertex3f(-2.8,0,2.8);
-    glVertex3f(-2.8,-0.3,2.8);
-    glVertex3f(-2.8,-0.3,-2.8);
-    glVertex3f(-2.8,0,-2.8);
-    
-    //right side
-    glColor3f(1, 0, 0.3);
-    glVertex3f(2.8,0,2.8);
-    glVertex3f(2.8,-0.3,2.8);
-    glVertex3f(2.8,-0.3,-2.8);
-    glVertex3f(2.8,0,-2.8);
-    
-    //back side
-    glColor3f(1, 0, 0.3);
-    glVertex3f(-2.8,0,-2.8);
-    glVertex3f(-2.8,-0.3,-2.8);
-    glVertex3f(2.8,-0.3,-2.8);
-    glVertex3f(2.8,0,-2.8);
-    
-	glEnd();
-    
-    //Section used to draw a black hole within the platform, one of the extra features
-    glPushMatrix();
-    
-    glTranslated(1, 0, 1);
-    glBegin(GL_QUADS);
-    glColor3f(0, 0, 0);
-    glVertex3f(-0.4,0,0.4);
-    glVertex3f(0.4,0,0.4);
-    glVertex3f(0.4,0,-0.4);
-    glVertex3f(-0.4,0,-0.4);
-    glEnd();
-    
-    glPopMatrix();
-    
+    //count through number of objects to perform tests on each one
+        //count through number of planes of each object to perform test on each one
+        for (int i = 0; i < 4; i++){
+            
+            
+            Get3DPos(x, y, 0.0, pNear);
+            Get3DPos(x, y, 1.0, pFar);
+            
+            //store ray orgin
+            newRay.org[0] = camera[0];
+            newRay.org[1] = camera[1];
+            newRay.org[2] = camera[2];
+            
+            //ray direction is the vector (pFar - pNear)
+            newRay.dir[0] = pFar[0] - pNear[0];
+            newRay.dir[1] = pFar[1] - pNear[1];
+            newRay.dir[2] = pFar[2] - pNear[2];
+            
+            //normalize direction vector
+            newRay.normalizeDirection();
+            
+            //undergoe ray plane test
+            groundPlane = rayPlaneTest(i);
+            
+            //update the position of the object to the intersection point
+            if ( groundPlane == true){
+                objectPos[0] = inter[0];
+                objectPos[1] = inter[1];
+                objectPos[2] = inter[2];
+                
+                //check if object is hit between min and max bounds
+                if ((minX < objectPos[0] && objectPos[0] < maxX && minZ < objectPos[2] && objectPos[2] < maxZ && objectPos[1] < maxY && minY < objectPos[1])){
+                    hit = true;
+                    break;
+                }
+                else{
+                    hit = false;
+                }
+     
+                
+            }
+        }  
 }
+            
+            
+            
+                                                                                                                              
 
 
 
@@ -219,7 +156,7 @@ void drawPlatform()
 
 void pinballStruct(){
     glBegin(GL_QUADS);
-    
+                                                                                                                              
     //platform
     glColor3f(0.3, 0, 1);
     glVertex3f(-1.5,-0.7,2.5);
@@ -238,9 +175,7 @@ void pinballStruct(){
     glVertex3f(-1.5,-0.6,-2.5);
     glVertex3f(-1.5,-0.7,-2.5);
     glVertex3f(1.5,-0.7,-2.5);
-    
 
-    
     //left side
     glColor3f(1, 0, 0.3);
     glVertex3f(-1.5,0,2.5);
@@ -334,6 +269,10 @@ void display(void)
     glLoadIdentity();
     gluLookAt(camera[0], camera[1], camera[2], 0, 0, 0, 0, 1, 0);
     glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+    
+    if (hit == true){
+    glutSolidTeapot(1);
+    }
     
     glPushMatrix();
     glRotatef(angY, 1, 0, 0);//rotate scene around x axis
@@ -446,7 +385,7 @@ void mouse(int btn, int state, int x, int y)
     //ray cast is pressed
 	if(btn == GLUT_LEFT_BUTTON)
 	{
-        //rayCast(x, y);
+        rayCast(x, y);
 	}
     //delete if pressed
 	if(btn == GLUT_RIGHT_BUTTON)
@@ -473,6 +412,7 @@ int main(int argc, char** argv)
 	gluPerspective(65, 1, 1, 100);
     
 	glMatrixMode(GL_MODELVIEW);
+    glutMouseFunc(mouse);
     
     glFrontFace(GL_CCW);
     glCullFace(GL_BACK);
